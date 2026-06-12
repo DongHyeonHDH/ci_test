95a6ddf18204967b0e1ad0b65f03544b9f4a12ee
황동현 <aaabbb4202@naver.com>
2026-06-12 09:52:28 +0900

    start

---
+++ src/main/java/io/nh_backend/ap_demo/testController.java
@@ -0,0 +1,12 @@
+package io.nh_backend.ap_demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class testController {
+
+    @GetMapping('/hello')
+    public String sayHello(){
+        return "Hello World!";
+    }
+
+}
