178a4b39963ce48cddbce94cf1d519def3ebbdd2
황동현 <aaabbb4202@naver.com>
2026-06-12 09:52:51 +0900

    start

---
+++ src/main/java/io/nh_backend/ap_demo/ApDemoApplication.java
@@ -0,0 +1,13 @@
+package io.nh_backend.ap_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/nh_backend/ap_demo/ApDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nh_backend.ap_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
